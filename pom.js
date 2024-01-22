0c30927073193652e20f417a034ac5e21b1f20d0
Joel Hughes <joel@nanopay.net>
2024-01-22 12:27:16 -0500

    initial build setup

---
+++ pom.js
@@ -0,0 +1,7 @@
+foam.POM({
+  name: 'demoapp',
+  projects: [
+    { name: 'foam3/pom' },
+    { name: 'src/demo/app/pom' }
+  ]
+});
---
+++ src/demo/app/pom.js
@@ -0,0 +1,7 @@
+foam.POM({
+  name: 'app',
+  files: [
+    { name: 'DemoApp',
+      flags: 'js|java' }
+  ]
+});
